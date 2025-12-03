9925e36e0045be9c5001368fcc05b89b73176e7c
Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>
2025-12-03 15:05:20 +0800

    hal: bee: add bee series files
    Signed-off-by: Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>

---
+++ bee/rtl87x2g/boot/inc/cpu_setting.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 Realtek Semiconductor Corp.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPU_SETTING_H
+#define CPU_SETTING_H
+
+/*============================================================================*
+ *                               Header Files
+*============================================================================*/
+#include "armv81mml_setting.h"                       /* Processor and core peripherals */
+
+#endif //#define CPU_SETTING_H
---
+++ bee/rtl87x2g/boot/inc/rtl876x.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 Realtek Semiconductor Corp.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef RTL876X_H
+#define RTL876X_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "address_map.h"
+#include "aon_reg.h"
+#include "bitops.h"
+#include "cpu_setting.h"
+#include "io.h"
+#include "pcc_reg.h"
+#include "platform_reg.h"
+#include "vector_table.h"
+
+
+/*============================================================================*
+ *                              Macros
+ *============================================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+#endif  /* RTL876X_H */
